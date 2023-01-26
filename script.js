a535189236cd821d27d595885702576522f526b7
xaviervamanda <amandavxavier@gmail.com>
2023-01-26 19:20:34 -0300

    Tela 3.2 inicial

--- script.js
+++ script.js
@@ -1,30 +1,94 @@
 let listaQuizzes;
 const url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes';
 
-const criarQuizzTitulo = document.querySelector('.titulo-quizz').value;
-const criarQuizzImagem = document.querySelector('.url-imagem').value;
-const criarQuizzQntPerguntas = document.querySelector('.qnt-perguntas').value;
-const criarQuizzQntNiveis = document.querySelector('.qnt-niveis').value;
+const tela1 = document.querySelector('.tela1');
+const tela3Parte1 = document.querySelector ('.tela3-parte-um');
+const tela3Parte2Inicial = document.querySelector(".tela3-parte-dois-incial");
 
-function criarPerguntas() {
-    const arry = ["1","2","3","4","5","6","7","8","9","0"];
-    if (criarQuizzTitulo !== ''&& criarQuizzImagem !== '' && criarQuizzQntPerguntas !== '' && criarQuizzQntNiveis !== ''){
-        // transformar a string qntperguntas e qntniveis em array de caracteres
-        // checar se cada caracter ta dentro do arry por um for
-        // checar se qnt perguntas >= 3
-        // obs: cada pergunta deve ter entre 2 a 4 respostas
-        // checar se qnt niveis >= 2
-        //Título do quizz: deve ter no mínimo 20 e no máximo 65 caracteres
-        // URL da Imagem: deve ter formato de URL
+let criarQuizzTitulo = document.querySelector('.titulo-quizz');
+let criarQuizzImagem = document.querySelector('.url-imagem');
+let criarQuizzQntPerguntas = document.querySelector('.qnt-perguntas');
+let criarQuizzQntNiveis = document.querySelector('.qnt-niveis');
 
+let checagem = [];
+
+function mostraCamposPergunta (){
+    alert ("Pode mostrar os campos da pergunta.");
+}
+
+function checaQntNiveisQuizz (){
+    // checar se qnt niveis >= 2
+    if (Number(criarQuizzQntNiveis.value) >= 2){
+        checagem.push("OK");
+    } else {
+        alert ("Insira uma quantidade de níveis válida. Deve ser um número e ser maior ou igual a 2.");
     }
+}
 
+function checaQntPerguntasQuizz (){
+    // checar se qnt perguntas >= 3
+    const arry = ["1","2","3","4","5","6","7","8","9","0"];
+    for (let i = 0; i < criarQuizzQntPerguntas.value.length; i++){
+        if (arry.includes(criarQuizzQntPerguntas.value[i]) === false || Number(criarQuizzQntPerguntas.value) < 3){
+            alert ("Insira uma quantidade de perguntas válida. Deve ser um número e ser maior ou igual a 3.");
+        } else {
+            checagem.push("OK");
+        }
+    }
+}
+
+function checaURLImagemQuizz (){
+    // URL da Imagem: deve ter formato de URL
+    // Quando tiver a forma certa de checar se a URL é válida, add um "OK" no array checagem
+    checagem.push ("OK");
+}
+
+function checaTituloQuizz (){
+    //Título do quizz: deve ter no mínimo 20 e no máximo 65 caracteres
+    const arrayTitulo = criarQuizzTitulo.value.split('');
+    if (arrayTitulo.length >= 25 && arrayTitulo.length <= 65){
+        checagem.push("OK");
+    } else {
+        alert ("Insira um título válido. Deve conter entre 25 e 60 caracteres.");
+    }
+}
+
+function mostrarPaginaCriacaoPerguntas() {
+    // obs: cada pergunta deve ter entre 2 a 4 respostas
+    checagem = [];
+    checaTituloQuizz ();
+    // A função abaixo precisa ser feita ainda. É a unica que não está funcionando.
+    checaURLImagemQuizz ();
+    checaQntPerguntasQuizz ();
+    checaQntNiveisQuizz ();
+    console.log (checagem);
+    if (checagem.length === 4){
+        for (i = 0; i < criarQuizzQntPerguntas.value; i++){
+            if (i === 0){
+                tela3Parte2Inicial.innerHTML += `
+                <h2>Crie suas Perguntas</h2>
+
+                <div class="caixa-pergunta">
+                    <p>Pergunta ${(i + 1)}</p>
+                    <ion-icon name="create-outline" onclick="mostraCamposPergunta ()"></ion-icon>
+                </div>
+                `;  
+            } else {
+              tela3Parte2Inicial.innerHTML += `
+            <div class="caixa-pergunta">
+                <p>Pergunta ${(i + 1)}</p>
+                <ion-icon name="create-outline" onclick="mostraCamposPergunta ()"></ion-icon>
+            </div>
+            `;  
+            }
+        }
+        tela3Parte1.classList.add ('esconder');
+        tela3Parte2Inicial.classList.remove('esconder');
+    }
 }
 
 function acessarTelaCriarQuizz (){
-    const tela1 = document.querySelector('.tela1');
     tela1.classList.add('esconder');
-    const tela3Parte1 = document.querySelector('.tela3-parte-um');
     tela3Parte1.classList.remove('esconder');
 }
 
@@ -70,7 +134,7 @@ function exibirQuizz(quizz){
     });
     
     promise.catch ( () => {
-        confirm ("Ocorreu um erro ao exibir o quizz. Recarregue a página.");
+        alert ("Ocorreu um erro ao exibir o quizz. Recarregue a página.");
         window.location.reload;
     })
 }
